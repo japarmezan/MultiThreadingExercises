90619e9d339cff24d25bf105142315c80d48e560
japarmezan <jani.michalkova@gmail.com>
2018-03-13 15:35:14 +0100

    Init commit

---
+++ Excercise/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // THREAD SYNC
+            //Console.WriteLine("*****Synchronizing Threads *****\n");
+            //Printer p = new Printer();
+            //// Make 10 threads that are all pointing to the same
+            //// method on the same object.
+            //Thread[] threads = new Thread[10];
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+            //    {
+            //        Name = $"Worker thread #{i}"
+            //    };
+            //}
+
+            //// Now start each one.
+            //foreach (Thread t in threads)
+            //    t.Start();
+
+
+            // THREAD INFO
+            // Prompt user for a PID and print out the set of active threads.
+            //Console.WriteLine("***** Enter PID of process to investigate *****");
+            //Console.Write("PID: ");
+            //string pID = Console.ReadLine();
+            //int theProcID = int.Parse(pID);
+            //EnumThreadsForPid(theProcID);
+
+            // START PROCESS
+            StartAndKillProcess();
+
+            Console.ReadLine();
+
+            
+        }
+
+        static void EnumThreadsForPid(int pID)
+        {
+            Process theProc = null;
+            try
+            {
+                theProc = Process.GetProcessById(pID);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            // List out stats for each thread in the specified process.
+            Console.WriteLine("Here are the threads used by: {0}", theProc.ProcessName);
+            ProcessThreadCollection theThreads = theProc.Threads;
+            foreach (ProcessThread pt in theThreads)
+            {
+                string info =
+                    $"-> Thread ID: {pt.Id}\tStart Time: {pt.StartTime.ToShortTimeString()}\tPriority:{ pt.PriorityLevel}";
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("************************************\n");
+        }
+
+        static void StartAndKillProcess()
+        {
+            Process ffProc = null;
+            // Launch Firefox, and go to Facebook!
+            try
+            {
+                ffProc = Process.Start("chrome.exe", "www.facebook.com");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Write("--> Hit enter to kill {0}...", ffProc.ProcessName);
+            Console.ReadLine();
+            // Kill the iexplore.exe process.
+            try
+            {
+                ffProc.Kill();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public class Printer
+    {
+        private static object threadLock = new object();
+
+        public void PrintNumbers()
+        {
+            // Use the private object lock token.
+            //lock (threadLock)
+            { 
+                // Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()",
+                Thread.CurrentThread.Name);
+                // Print out numbers.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(10 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,180 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private static string baseUri = "https://en.wikipedia.org";
+        private static Uri startUrl = new Uri("https://en.wikipedia.org/wiki/USS_Lexington_(CV-2)");
+
+        //private static string baseUri = "https://golang.org";
+        //private static Uri startUrl = new Uri("https://golang.org/");
+        private static int successfulUrisCount;
+
+        private static ConcurrentQueue<VisitedUri> consumerQueue = new ConcurrentQueue<VisitedUri>();
+        private static ConcurrentBag<VisitedUri> allProcessedUris = new ConcurrentBag<VisitedUri>();
+
+        private static int CRAWL_DEPTH = 1;
+        private static int CHECK_TIMEOUT = 2000;
+
+        private struct VisitedUri
+        {
+            public Uri Uri;
+            public int depth;
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is VisitedUri))
+                    return false;
+
+                var other = (VisitedUri) obj;
+
+                return other.Uri.Equals(this.Uri);
+            }
+
+            public override int GetHashCode()
+            {
+                return Uri.GetHashCode();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("And awaaaaaaaay we go!");
+
+            var timerCallback = new TimerCallback(CheckState);
+
+            var _ = new Timer(timerCallback, null, CHECK_TIMEOUT, CHECK_TIMEOUT);
+
+            ProcessEntry(startUrl, 0);
+
+            while (true)
+            {
+                var dequeuedSuccesfully = consumerQueue.TryDequeue(out var processUri);
+
+                if (dequeuedSuccesfully)
+                {
+                    var newTask = new Task(() => ProcessEntry(processUri.Uri, processUri.depth + 1));
+                    newTask.Start();
+                }
+            }
+        }
+
+        private static void CheckState(object state)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine(consumerQueue.IsEmpty
+                ? $"******* QUEUE EMTPY crawled through {allProcessedUris.Count} pages"
+                : "******* Checking queue");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Number of threads: {Process.GetCurrentProcess().Threads.Count}");
+
+            Console.ResetColor();
+        }
+
+        private static void ProcessEntry(Uri url, int depth)
+        {
+            if (depth > CRAWL_DEPTH)
+                return;
+
+            HtmlWeb hw = new HtmlWeb();
+            HtmlDocument doc = new HtmlDocument();
+
+            try
+            {
+                doc = hw.Load(url);
+                Interlocked.Increment(ref successfulUrisCount);
+
+                var allLinks = FindAllLinksInPage(doc);
+                EnqueueEntries(allLinks.ToList(), depth);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR could not load {url}: {e.Message}");
+            }
+        }
+
+        private static object bagLock = new object();
+
+        private static IEnumerable<string> FindAllLinksInPage(HtmlDocument doc)
+        {
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                var linkHrefValue = link.Attributes.FirstOrDefault(x => x.Name.Equals("href"))?.Value;
+                if (string.IsNullOrEmpty(linkHrefValue))
+                    continue;
+
+                if (!ValidateUrl(linkHrefValue))
+                    continue;
+
+                if (linkHrefValue[0] != '/')
+                    linkHrefValue = linkHrefValue.Insert(0, "/");
+
+                var foundUri = baseUri + linkHrefValue;
+
+                yield return foundUri;
+            }
+        }
+
+        private static void EnqueueEntries(List<string> urls, int depth)
+        {
+            foreach (var url in urls)
+            {
+                if (TryCreateEntry(out var entry, url, depth))
+                    TryEnqueueEntry(entry);
+            }
+        }
+
+        private static bool ValidateUrl(string linkHrefValue)
+        {
+            return !linkHrefValue.StartsWith("//") && !linkHrefValue.StartsWith("#") && !linkHrefValue.StartsWith("http") && !linkHrefValue.Contains("www") && !linkHrefValue.Contains(".com");
+        }
+
+        private static bool TryEnqueueEntry(VisitedUri entry)
+        {
+            lock (bagLock)
+            {
+                if (allProcessedUris.Contains(entry))
+                    return false;
+            }
+
+            allProcessedUris.Add(entry);
+            consumerQueue.Enqueue(entry);
+            // Console.WriteLine(entry.Uri);
+
+            return true;
+        }
+
+        private static bool TryCreateEntry(out VisitedUri entry, string foundUri, int depth)
+        {
+            try
+            {
+                entry = new VisitedUri
+                {
+                    Uri = new Uri(foundUri),
+                    depth = depth
+                };
+
+            }
+            catch (UriFormatException e)
+            {
+                Console.WriteLine($"Wrong URI format: {foundUri}");
+                entry = new VisitedUri();
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
